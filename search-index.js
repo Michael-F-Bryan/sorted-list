86161565c52c422e86d9bc21778e48cbf71f5eb4
Michael-F-Bryan <Michael-F-Bryan@users.noreply.github.com>
2020-12-20 11:46:54 +0000

    Deploy to GitHub pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"sorted_list":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["sorted_list"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
